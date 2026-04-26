ca5a3769f26fa91eb9c0b10188a5049cfd6eaa7f
Lezgyan <lezgyanandrew@gmail.com>
2026-04-26 19:02:57 +0400

    fix: залогировал ошибку

--- src/main/java/ru/tbank/practicum/job/ScheduledHouseRules.java
+++ src/main/java/ru/tbank/practicum/job/ScheduledHouseRules.java
@@ -1,12 +1,14 @@
 package ru.tbank.practicum.job;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ru.tbank.practicum.metrics.HouseRulesMetrics;
 import ru.tbank.practicum.service.AutomationService;
 
 @RequiredArgsConstructor
+@Slf4j
 @Component
 public class ScheduledHouseRules {
 
@@ -21,7 +23,7 @@ public class ScheduledHouseRules {
             metrics.incrementSuccess();
         } catch (Exception e) {
             metrics.incrementError();
-            e.printStackTrace();
+            log.error("Не удалаось применить правила", e);
         }
     }
 }
